fae24d3da4406079241b5b4d31fc684e3f3a1810
Paul Keahi <paulkeahi@gmail.com>
2015-08-29 15:30:12 -1000

    Added Constant with (dollarsign)rootScope

--- public/js/myController.controller.js
+++ public/js/myController.controller.js
@@ -1,8 +1,7 @@
 (function (){
   angular
   .module('myApp')
-  .controller('myController', ['$scope', 'myAppTitle', 'APP_VERSION', function($scope, myAppTitle, APP_VERSION){
-    console.log(APP_VERSION);
+  .controller('myController', ['$scope', 'myAppTitle', function($scope, myAppTitle){
     $scope.myModel = "Hello";
     $scope.title = myAppTitle;
   }]);
