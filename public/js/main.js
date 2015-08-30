d8bf7070cb85af16fafcff08b6a08c90304b2920
Paul Keahi <paulkeahi@gmail.com>
2015-08-29 19:58:49 -1000

    added routes with ngRoute, need to create more html files

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,29 @@
 (function (){
-  var app = angular.module('myApp', ['ngResource']);
+  var app = angular.module('myApp', ['ngResource', 'ngRoute']);
+
+  app.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+      .when('/books', {
+        templateUrl: 'views/books.html',
+        controller: 'BookController'
+      })
+      .when('/movies', {
+        templateUrl: 'views/movies.html',
+        controller: 'MovieController'
+      })
+      .when('/other', {
+        templateUrl: 'views/other.html',
+        controller: 'myController'
+      })
+      .when('/', {
+        templateUrl: 'views/default.html',
+        controller: 'myController'
+      });
+  }]);
 
   app.run(function($rootScope, APP_VERSION){
     $rootScope.APP_VERSION = APP_VERSION;
   });
-
 
 
 })();
