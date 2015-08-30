260e18eada5479cedb2c52b0b91418401823204f
Paul Keahi <paulkeahi@gmail.com>
2015-08-29 14:25:46 -1000

    moved controller to its own file

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,3 @@
-var app = angular.module('myApp', []);
-
-app.controller('myController', ['$scope', function($scope){
-  $scope.myModel = "Hello";
-}]);
+(function (){
+  var app = angular.module('myApp', []);
+})();
